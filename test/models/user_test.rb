4dd14745ab3a93b845c310a59ab97cf6cc4116be
Daniel Jackson <jacksonvoice@yahoo.com>
2014-09-06 08:26:48 -0700

    fixed error with reuglar expresson on profile name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,19 +24,27 @@ class UserTest < ActiveSupport::TestCase
 
 	test "a user should have a unique profile name" do
 		user = User.new
-		user.profile_name = users(:one).profile_name
+		user.profile_name = users(:daniel).profile_name
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 	end
 
 	test "a user should have a provile name without spaces" do
-		user = User.new
+		user = User.new(first_name: 'Daniel', last_name: 'Jackson', email: 'jacksonvoice2@yahoo.com' )
+    user.password = user.password_confirmation = 'asdfasdf'
 		user.profile_name = "My profile with spaces"
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
 	end
 
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Daniel', last_name: 'Jackson', email: 'jacksonvoice2@yahoo.com' )
+    user.password = user.password_confirmation = 'asdfasdf'
+    user.profile_name = 'jacksonvoice_1'
+    assert user.valid?
+
+  end
 
 
 
