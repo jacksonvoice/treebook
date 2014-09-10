c6f5e2f683055666a74932ff0d08beecc190696e
Daniel Jackson <jacksonvoice@yahoo.com>
2014-09-10 15:06:25 -0700

    generated user_friendships controller, testing relationhships betweens users as friends

---
+++ app/controllers/user_friendships_controller.rb
@@ -0,0 +1,9 @@
+class UserFriendshipsController < ApplicationController
+
+
+private
+	
+	# def user_friendship_params
+	# 	params.require(:friend).permit(:friend_id, :user_id)
+	# end
+end
--- test/models/user_friendship_test.rb
+++ test/models/user_friendship_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	should belong_to(:user)
+	should belong_to(:friend)
+
+	test "that creating a user friendship is successful without raising an exception" do
+		assert_nothing_raised do
+		UserFriendship.create user: users(:daniel), friend: users(:olivia)
+	end
+	
+  end
+
 end
