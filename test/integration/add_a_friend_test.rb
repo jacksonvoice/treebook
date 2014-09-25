670d7a052a0f04c4014ea7c77f60636005fe13e0
Daniel Jackson <jacksonvoice@yahoo.com>
2014-09-25 15:03:26 -0700

    fixed add_a_friend test, currently passing

--- test/integration/add_a_friend_test.rb
+++ test/integration/add_a_friend_test.rb
@@ -8,7 +8,14 @@ class AddAFriendTest < ActionDispatch::IntegrationTest
 
 	test "that adding a friend works" do 
 		sign_in_as users(:daniel), "testing"
+
 		get "/user_friendships/new?friend_id=#{users(:olivia).profile_name}"
 		assert_response :success
+
+		assert_difference 'UserFriendship.count' do 
+			post "/user_friendships", user_friendship: { friend_id: users(:olivia).profile_name }
+			assert_response :redirect
+			assert_equal "You are now friends with #{users(:olivia).full_name}!", flash[:success]
+		end
 	end
 end
